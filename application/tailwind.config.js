4af1379df65513a8e6b915c986e9612608c8c705
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-09-23 20:05:55 +0100

    new app configured due to taiwind issues | navbar created

---
+++ application/tailwind.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.jsx",
+    "./src/Navbar.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
