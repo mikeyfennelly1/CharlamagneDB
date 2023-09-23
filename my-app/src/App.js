cb53f0352902050998b49c9b25847b95ac9c5ba3
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-09-23 18:55:08 +0100

    react app with tailwind set up

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Navbar.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        <>
+        <div className="Navbar">
+            <h1>CharlemagneDB</h1>
+            <div className="center3">
+                <h3>Products</h3>
+                <h3>Docs</h3>
+                <h3>Solutions</h3>
+            </div>
+            <button>Login</button>
+        </div>
+        </>
+    );
+}
+
+export default Navbar;
