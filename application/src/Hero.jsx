467f7c6897ee994644aeb153e5dfd595a5c44e2d
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-09-23 21:07:53 +0100

    rest of text content added to hero

--- application/src/Hero.jsx
+++ application/src/Hero.jsx
@@ -1,8 +1,15 @@
 function Hero() {
     return(
         <>
-        <div className="max-w-fit mx-auto mt-[35vh]">
-            <h1 className="text-mainPink font-[TT-Travels-Next-Light] mx-auto text-4xl">The emperor of all databases.</h1>
+        <div className="max-w-fit mx-auto mt-[35vh] flex flex-col">
+            <h1 className="text-mainPink font-[TT-Travels-Next-Light] mx-auto text-5xl mb-10">The emperor of all databases.</h1>
+            <h3 className="font-[TT-Autonomous-Mono-Reg] text-white mx-auto text-xl text-center">Find out for yourself why Charlemagne was voted as the most loved <br/> NoSQL database by developers in 2025.</h3>
+            <button className="font-[TT-Autonomous-Mono-Reg] text-white mx-auto text-lg text-center border-2 rounded-full py-2 px-5 mt-7
+                             hover:bg-white hover:text-black hover:font-black">
+                Try For Free</button>
+        </div>
+        <div className="max-w-fit mx-auto mt-[35vh] flex flex-col">
+            <h3 className="text-white px-auto hover:cursor-pointer">Learn More about Charlemagne.</h3>
         </div>
         </>
     );
--- application/src/Navbar.jsx
+++ application/src/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
     return(
         <>
         <div className="flex flex-wrap justify-around mx-auto mt-7 text-white max-w-[1500px] min-w-1500px ">
-            <h1 className="">CharlemagneDB</h1>
+            <h1 className="font-[TT-Travels-Next-Med] text-mainPink">CharlemagneDB</h1>
                 <div className="flex gap-20 bg-[#212121] px-7 py-2 rounded-md">
                     <h3 className="">Products</h3>
                     <h3 className="">Solutions</h3>
