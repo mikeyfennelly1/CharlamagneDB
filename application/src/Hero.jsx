fc16d55ccccb16e6df1cdfd83a2e9e7b1e4a6c8c
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-09-23 20:41:37 +0100

    forgot these in the last commit

---
+++ application/src/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return(
+        <>
+        <div className="max-w-fit mx-auto mt-[35vh]">
+            <h1 className="text-mainPink font-[TT-Travels-Next-Light] mx-auto text-4xl">The emperor of all databases.</h1>
+        </div>
+        </>
+    );
+}
+
+export default Hero;
