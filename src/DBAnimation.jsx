417e5e989a81c0ca8919d5c8f2ad1fe69ebbfb4c
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-10-13 18:52:35 +0100

    database issue with error resolved | beginning animation of 5 disks now

--- src/DBAnimation.jsx
+++ src/DBAnimation.jsx
@@ -1,6 +1,8 @@
 import {  useScroll, motion, useMotionValueEvent, useTransform, AnimatePresence} from 'framer-motion';
+import { useRef } from 'react';
 
 function DBAnimation() {
+    const targetRef = useRef(null);
     const { scrollYProgress, scrollY } = useScroll();
     useMotionValueEvent(scrollY, "change", (latest) => {
         console.log("Page scroll: ", latest)
@@ -9,30 +11,17 @@ function DBAnimation() {
 
       const  scaleProgress = useTransform(scrollYProgress, [0, 0.5], [0, 0.25]);
       const positionProgress = useTransform(scrollYProgress, [0, 0.5], [0, 0.25]);
+
+      const y = useTransform(
+        scrollYProgress,
+        [0.1, 0.25, 0.7, 1],
+        ["0%", "-55%", "-60%", "-80%"]
+      );
+
     return (
         <>
-        <section className="">
-            {/* <motion.div style={{ scaleY: scaleProgress }}
-                        whileHover={{ scale: 1.2 }}
-                        className="fixed rounded-full border-2 w-[400px] h-[400px] mx-[calc(50vw-200px)] top-[calc(47vh-200px)] opacity-10
-                            sm:w-[600px] sm:h-[600px] sm:mx-[calc(50vw-300px)] sm:top-[calc(50vh-300px)]">
-            </motion.div>
-            <motion.div style={{ scaleY: scaleProgress }}
-                        whileHover={{ scale: 1.2 }}
-                        className="fixed rounded-full border-2 w-[400px] h-[400px] mx-[calc(50vw-200px)] top-[calc(47vh-200px)] opacity-10
-                            sm:w-[600px] sm:h-[600px] sm:mx-[calc(50vw-300px)] sm:top-[calc(50vh-300px)]">
-            </motion.div>
-            <motion.div style={{ scaleY: scaleProgress }}
-                        whileHover={{ scale: 1.2 }}
-                        className="fixed rounded-full border-2 w-[400px] h-[400px] mx-[calc(50vw-200px)] top-[calc(47vh-200px)] opacity-10
-                            sm:w-[600px] sm:h-[600px] sm:mx-[calc(50vw-300px)] sm:top-[calc(50vh-300px)]">
-            </motion.div>
-            <motion.div style={{ scaleY: scaleProgress }}
-                        whileHover={{ scale: 1.2 }}
-                        className="fixed rounded-full border-2 w-[400px] h-[400px] mx-[calc(50vw-200px)] top-[calc(47vh-200px)] opacity-10
-                            sm:w-[600px] sm:h-[600px] sm:mx-[calc(50vw-300px)] sm:top-[calc(50vh-300px)]">
-            </motion.div> */}
-            <motion.div style={{ scaleY: scaleProgress }}
+        <section ref={targetRef} className="">
+            <motion.div style={{ scaleY: scaleProgress, y }}
                         
                         whileHover={{ scale: 1.2 }}
                         className="fixed rounded-full border-2 w-[400px] h-[400px] mx-[calc(50vw-200px)] top-[calc(47vh-200px)] opacity-10
