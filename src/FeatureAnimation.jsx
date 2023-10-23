190b58ced0ea181491efc0e103e5b8328398cec9
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-10-23 15:49:00 +0100

    logic for scroll animation done, words still split up tho

--- src/FeatureAnimation.jsx
+++ src/FeatureAnimation.jsx
@@ -28,12 +28,27 @@ const FeatureAnimation = () => {
             }
         });
 
-
     const opacity = useTransform(scrollYProgress, [0.25, 0.33], [0, 1])
 
 
-    
+    const phrase = "Charlemagne is designed with speed in mind, in query simplicity, an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast."
 
+    const splitWords = () => {
+        let body = []
+        phrase.split(" ").forEach( (word, index) => {
+            const letters = splitLetters(word);
+            body.push(<p key={`word_${index}`}>{letters}</p>)
+        })
+        return body;
+    }
+
+    const splitLetters = (word) => {
+        let letters = [];
+        word.split("").forEach( (letter, index) => {
+            letters.push(<span key={`letter_${index}`}>{letter}</span>)
+        })
+        return letters;
+    }
 
 
     return (
@@ -42,9 +57,13 @@ const FeatureAnimation = () => {
             <motion.div style={{ opacity, position, top, marginTop}} className="left-[calc(50vw-390px)] text-white max-w-[800px] justify-center">
                 <motion.h1 className='text-white text-3xl font-[TT-Autonomous-Mono-Reg] max-w-fit mx-auto'>BLAZING FAST DEVELOPMENT WORKFLOW.</motion.h1>
                 <motion.h2 className='text-white text-2xl text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light'>
-                    Charlemagne is designed with speed in mind, in query simplicity, an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</motion.h2>
+                    {
+                        splitWords()
+                    }
+                    </motion.h2>
             </motion.div>
         </motion.section>
+
         </>
     );
 }
