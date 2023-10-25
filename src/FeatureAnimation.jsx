f5b727ecd54516203e141423283f0e98e294c4ec
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-10-25 20:13:26 +0100

    text animation done with GSAP

--- src/FeatureAnimation.jsx
+++ src/FeatureAnimation.jsx
@@ -1,6 +1,8 @@
 import { useScroll, motion, useMotionValueEvent, useTransform } from 'framer-motion';
-import { useRef, useState } from 'react';
-import Typewriter from 'typewriter-effect';
+import { useRef, useState, useEffect } from 'react';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
                     
 const FeatureAnimation = () => {
     const blazingFast = useRef(null);
@@ -30,14 +32,37 @@ const FeatureAnimation = () => {
 
     const opacity = useTransform(scrollYProgress, [0.25, 0.33], [0, 1])
 
+    ///////////////////////
 
     const phrase = "Charlemagne is designed with speed in mind, in query simplicity, an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast."
+    
+    const refs = useRef([])
+    const container = useRef(null)
+    useEffect( () => {
+        gsap.registerPlugin(ScrollTrigger);
+        createAnimations()
+    }, [])
+
+    const createAnimations = () => {
+        gsap.to(refs.current, {
+            opacity: 1,
+            ease: "none",
+            stagger: 0.1,
+            scrollTrigger: {
+                trigger: container.current,
+                start: 'start',
+                end: "+=800",
+                markers: true,
+                scrub: true
+            },
+        })
+    }
 
     const splitWords = () => {
         let body = []
         phrase.split(" ").forEach( (word, index) => {
             const letters = splitLetters(word);
-            body.push(<p key={`word_${index}`}>{letters}</p>)
+            body.push(<div key={`word_${index}`} className='mr-4 inline-block'>{letters}</div>)
         })
         return body;
     }
@@ -45,22 +70,21 @@ const FeatureAnimation = () => {
     const splitLetters = (word) => {
         let letters = [];
         word.split("").forEach( (letter, index) => {
-            letters.push(<span key={`letter_${index}`}>{letter}</span>)
+            letters.push(<span ref={el => {refs.current.push(el)}} key={`letter_${index}`} className='opacity-20'>{letter}</span>)
         })
         return letters;
     }
 
+    ///////////////////////
 
     return (
         <>
         <motion.section ref={blazingFast} className="min-h-[200vh]">
-            <motion.div style={{ opacity, position, top, marginTop}} className="left-[calc(50vw-390px)] text-white max-w-[800px] justify-center">
-                <motion.h1 className='text-white text-3xl font-[TT-Autonomous-Mono-Reg] max-w-fit mx-auto'>BLAZING FAST DEVELOPMENT WORKFLOW.</motion.h1>
-                <motion.h2 className='text-white text-2xl text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light'>
-                    {
-                        splitWords()
-                    }
-                    </motion.h2>
+            <motion.div ref={container} style={{ opacity, position, top, marginTop }} className="min-h-[400px] left-[calc(50vw-600px)] text-white max-w-[1200px]">
+                <motion.h1 className='text-mainPink text-5xl font-[TT-Autonomous-Mono-Reg] max-w-fit mx-auto'>BLAZING FAST DEVELOPMENT WORKFLOW.</motion.h1>
+                <motion.div className='text-white text-2xl text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light flex flex-wrap height-fit'>
+                    {splitWords(phrase)}
+                    </motion.div>
             </motion.div>
         </motion.section>
 
