d4bd597b22b383bab5c2b768573d9e8dfeca4550
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-11-13 20:33:10 +0000

    companies that use... section adapted for smaller screens

--- src/6.jsx
+++ src/6.jsx
@@ -2,12 +2,14 @@ const Six = () => {
     return (
         <>
             <div className="">
-                <div className="min-h-[400px] max-w-[1600px] mx-auto">
-                    <div className="text-white text-[1.3rem] font-[TT-Travels-Next-Med]  font-thin border-2 rounded-full max-w-fit max-h-fit px-6 py-2 mx-auto">
+                <div className="min-h-[400px] max-w-[1600px] lg:mx-auto">
+                    <div className="text-white  font-[TT-Travels-Next-Med] mx-[50px] font-thin border-2 rounded-full max-w-fit max-h-fit px-6 py-2
+                                    lg:text-[1.3rem] lg:mx-auto">
                         Companies using Charlemagne as their Primary DB
                     </div>
-                    <div className="mt-[100px]">
-                        <img src="../images/logosTemplate.png" alt="" className=""/>
+                    <div className="mt-[50px] overflow-x-hidden border-2 relative
+                                    lg:mt-[100px]">
+                        <img src="../images/logosTemplate.png" alt="" className="min-h-[200px] "/>
                     </div>
                 </div>
             </div>
