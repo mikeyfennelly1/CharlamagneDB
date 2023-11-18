d380bfdb254ddaa248aec3f4740418ef241307a6
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-11-18 23:54:56 +0000

    more logos added | section 7 basics done

--- src/6.jsx
+++ src/6.jsx
@@ -3,17 +3,23 @@ const Six = () => {
         <>
             <div className="">
                 <div className="max-w-[1600px] lg:mx-auto">
-                    <div className="text-white font-[TT-Travels-Next-Med] mx-[50px] font-thin border-2 rounded-full max-w-fit max-h-fit px-6 py-2 text-center
+                    <div className="text-mainPink font-[TT-Travels-Next-Med] mx-[50px] font-thin border-2 rounded-full max-w-fit max-h-fit px-6 py-2 text-center
                                     lg:text-[1.3rem] lg:mx-auto">
                         Companies using Charlemagne as their Primary DB
                     </div>
-                    <div className=" min-h-[400px] grid grid-cols-4 items-center mt-[100px]">
+                    <div className=" min-h-[400px] grid grid-cols-4 items-center my-[150px]">
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/aws-logo 1.png" alt="" />
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/bloomberg-logo-white 1.png" alt="" />
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/intelLogo 1.png" alt="" />
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/openaiLogo 1.png" alt="" />
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/shopifyLogo 1.png" alt="" />
             <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/youtubeLogo 1.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/jpMorganChaseLogo.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/bostonScientificLogo.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/deloitteLogo.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/stripeLogo.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/salesForceLogo.png" alt="" />
+            <img className="borer-2 border-green max-h-[100px] max-h-[100px] inline mx-auto pb-[50px]" src="../images/companyLogos/asosLogo.png" alt="" />
                     </div>
                 </div>
             </div>
--- src/7.jsx
+++ src/7.jsx
@@ -3,8 +3,13 @@
 const Seven = () => {
     return (
         <>
-            <div className="border-2 border-white max-w-screen">
+            <div className="max-w-screen">
                 <div className="min-h-[200px]">
+                    <div className="max-w-fit mb-[100px] mx-auto">
+                        <img src="../images/officePhoto.jpg" alt="" className="max-h-[800px] rounded-[50px] inline opacity-60"/>
+                        <h1 className=" ml-[50px] text-[3rem] mt-[75px] text-mainPink font-[TT-Travels-Next-Med]">Work at CharlemagneDB</h1>
+                    <h1 className=" ml-[50px] text-[1.8rem] my-[50px] text-white font-[TT-Autonomous-Mono-Reg] border-2 border-white max-w-fit px-[20px] rounded-full opacit">View Job Openings</h1>
+                    </div>
                 </div>
             </div>
         </>
