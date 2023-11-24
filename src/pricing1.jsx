bda9fd88585ee25cc57c3498f2e7adaa5c7b1167
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-11-24 12:27:24 +0000

    pricing page classes states added

--- src/pricing1.jsx
+++ src/pricing1.jsx
@@ -4,15 +4,25 @@ import Five from './5.jsx'
 import CardsEnterPrise from './pricingCards-Enterprise.jsx';
 
 const Pricing1 = () => {
+    
+    const activeClasses = "cursor-pointer max-w-[400px] py-[30px] px-[50px] rounded-[10px] bg-[#0F0F0F] border-2 border-mainPink";
+    const inactiveClasses = "cursor-pointer max-w-[400px] py-[30px] px-[50px] rounded-[10px] bg-[#0F0F0F]";
 
+    const [activityStateA, setActivityStateA] = useState(activeClasses)
+    const [activityStateB, setActivityStateB] = useState(inactiveClasses)
+    
     const [state, setState] = useState(<CardsLoco/>);
 
-    const handleCardsLocoCLick = () => {
+    const clickA = () => {
         setState(<CardsLoco/>)
+        setActivityStateA(activeClasses)
+        setActivityStateB(inactiveClasses)
     }
 
-    const handleCardsEnterpriseCLick = () => {
+    const clickB = () => {
         setState(<CardsEnterPrise/>)
+        setActivityStateA(inactiveClasses)
+        setActivityStateB(activeClasses)
     }
 
     return (
@@ -23,11 +33,11 @@ const Pricing1 = () => {
                     <h3 className="text-[1.4rem] max-w-fit mx-auto">The data platform built by - and for - developers</h3>
                 </div>
                 <div className="grid grid-cols-2 max-w-[1000px] mx-auto pt-[100px]">
-                    <div onClick={handleCardsLocoCLick} className="cursor-pointer max-w-[400px] py-[30px] px-[50px] rounded-[10px] bg-[#0F0F0F] border-2 border-mainPink">
+                    <div onClick={clickA} className={activityStateA}>
                         <h1 className="text-mainPink font-[TT-Travels-Next-Med]">Loco</h1>
                         <h2>Developer Data Platform</h2>
                     </div>
-                    <div onClick={handleCardsEnterpriseCLick} className="cursor-pointer max-w-[400px] py-[30px] px-[50px] rounded-[10px] bg-[#0F0F0F]">
+                    <div onClick={clickB} className={activityStateB}>
                         <h1 className="text-mainPink font-[TT-Travels-Next-Med]">Enterprise</h1>
                         <h2>Self-Managed Database & Services</h2>
                     </div>
