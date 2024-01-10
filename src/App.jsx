8da9a31b4881cc0c2556f742b6e55ddcf4e8cba0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-10 14:15:34 +0000

    dead code removed

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ import Login from './login.jsx'
 import Pricing from './pricing.jsx'
 import Docs from './docs.jsx'
 
-import { useEffect } from 'react';
-// import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
