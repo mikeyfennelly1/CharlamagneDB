3fc2065db35d068d8968781c0d2fe72c7ba2e6ae
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-10 12:19:44 +0000

    react strict mode removed

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  //   <App />
+    <App />
   // </React.StrictMode>
-
-  <div>HEllo</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
