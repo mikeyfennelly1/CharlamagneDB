7f8e8d7c89b7d9852269b35cd68cbb25c19ccbca
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-11 18:13:34 +0000

    links in Navbar component chagned to CharlamangeDB/*

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return(
         <>
-        <section className=" md:block z-100 absolute top-0 text-white">
-            <div className="z-9999 absolute flex flex-wrap justify-between max-w-[100vw] w-[99vw] py-5 bg-black grid grid-cols-3">
+        <section className=" md:block z-1000 absolute top-0 text-white">
+            <div className="absolute flex flex-wrap justify-between max-w-[100vw] w-[99vw] py-5 bg-black grid grid-cols-3">
                 <div className="mx-auto flex items-center w-[200px] justify-start ml-[100px]">
                     <img className="max-h-[30px]" src="./images/Vector 88.png" alt="" />
-                    <Link to="/" className="font-[TT-Travels-Next-Med] cursor-pointer text-mainPink ml-[10px]">CharlemagneDB</Link>
+                    <Link to="/CharlamagneDB/" className="font-[TT-Travels-Next-Med] cursor-pointer text-mainPink ml-[10px]">CharlemagneDB</Link>
                 </div>
                 <div className="mx-auto gap-20 bg-[#212121] px-7 h-10 rounded-md hidden pb-0 max-h-10 max-w-fit font-[TT-Autonomous-Mono-Reg]
                                 lg:flex lg:items-center">
-                    <Link to="/loco" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Loco</Link>
-                    <Link to="/pricing" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Pricing</Link>
-                    <Link to="/docs" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Docs</Link>
+                    <Link to="/CharlamagneDB/loco" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Loco</Link>
+                    <Link to="/CharlamagneDB/pricing" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Pricing</Link>
+                    <Link to="/CharlamagneDB/docs" className="hover:cursor-pointer inline px-7 hover:text-mainPink">Docs</Link>
                 </div>
                 <div className="ml-auto items-center w-[200px] justify-end hidden self-center font-[TT-Autonomous-Mono-Reg]
                                 lg:block">
-                    <Link to="/login" className=" px-7 rounded-full hover:text-mainPink">Log In</Link>
+                    <Link to="/CharlamagneDB/login" className=" px-7 rounded-full hover:text-mainPink">Log In</Link>
                 </div>
                 {/* <div className="flex justify-end items-center min-w-[200px]
                                 lg:hidden">
