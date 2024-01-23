eb14ee5b55b2e331351009f898d5ad3bc023271a
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-23 20:35:41 +0000

    dashboard content aligned a bit better

--- src/dashboardMain.jsx
+++ src/dashboardMain.jsx
@@ -1,9 +1,20 @@
 const DashboardMain = () => {
     return (
-        <>
-                    <h1 className="pl-[20px] border max-w-[200px] text-white inline-block">Overview</h1>
+        <>       
+        <div className="text-white max-w-[1400px] min-h-[200px] inline-block px-[200px] py-[100px]">
+            <h1 className="text-mainPink text-2xl font-[TT-Travels-Next-Reg]">Overview</h1>
+            <p className="text-white font-[TT-Autonomous-Mono-Reg]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis eligendi ad ipsam aliquam voluptates nisi officiis numquam a sint! Dolor, eligendi consectetur. Eaque hic ipsam consequuntur voluptatum. Placeat, accusamus!
+                Natus voluptates at minus assumenda? Distinctio praesentium ducimus amet cupiditate, soluta consequatur eveniet excepturi, molestias quasi voluptas laudantium dolor inventore cumque aliquam unde nam? Optio tempora quas itaque perferendis. Omnis.
+                Nemo expedita, officia repudiandae unde voluptatibus accusantium ipsa est porro amet similique aliquid. Ipsam a explicabo dignissimos consequuntur. Suscipit, quae voluptatem quis sed quidem veritatis sequi dolor blanditiis cumque quo.
+                Temporibus sapiente eaque dolores veritatis consequatur reprehenderit ullam accusamus est sed quos id saepe mollitia, libero possimus reiciendis suscipit. Iste perspiciatis doloribus id libero? Numquam autem iste nulla adipisci accusantium?
+                Eos modi porro assumenda praesentium rerum quo perspiciatis quidem! Recusandae autem deserunt corporis adipisci veritatis molestias, odit atque quaerat nemo quod? Quo ratione laudantium sapiente, quia facilis cupiditate nemo officiis?
+                Natus minima qui exercitationem? Aliquam illum pariatur eius, aliquid id et voluptatem, nesciunt ipsa vel doloremque eligendi ipsum? Autem reiciendis laboriosam voluptatum molestiae eligendi eaque illo neque est eveniet modi.
+                Illum dolor magni laboriosam dicta unde voluptates, vel aperiam architecto tenetur amet error aliquid repellat hic cumque officia? Pariatur ex provident quod debitis ea adipisci autem obcaecati porro inventore consequuntur!
+            </p>
+        </div>
         </>
     );
 }
- 
+
 export default DashboardMain;
