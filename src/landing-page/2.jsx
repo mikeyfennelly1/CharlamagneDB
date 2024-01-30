389b64787012a6a721ef37ae183f3e2908c1a5b1
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-30 16:28:46 +0000

    text animation working

--- src/landing-page/2.jsx
+++ src/landing-page/2.jsx
@@ -1,32 +1,41 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
+import { TextPlugin } from "gsap/TextPlugin";
 import { useGSAP } from "@gsap/react";
 import BlazingFastText from './blazingFastText'
 
 gsap.registerPlugin(ScrollTrigger) 
+gsap.registerPlugin(TextPlugin)
 
 const FeatureAnimation = () => {
         useGSAP(() => {
-    
+            gsap.registerPlugin(SplitText);
             const tl = gsap.timeline({
                 scrollTrigger: {
                   pin: true,
                   trigger: "#outerDiv",
                   start: "top 25%",
                   markers: true,
-                  pinReparent: true,
                   end: "bottom 25%",
+                  scrub: true,
                 },
               });
-            
+              var split = new SplitText("#typingText", {type: "chars, words"});
+              //now animate each character into place from 100px above, fading in:
+              tl.to("#typingText", {
+                text: "Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.",
+                ease: "none",
+                duration: 2,
+              });
         })
     return (
         <>
         <div id='outerDiv' className="h-[1000px]
-        max-w-screen mb-[150px] flex justify-center border-[20px] border-mainPink">
+        max-w-screen mb-[150px] flex justify-center">
         
             <div id='innerDiv' className="
-            border-8 border-[#F00] max-h-[400px] 2xl:max-w-[1600px] 2xl:min-w-[1600px]
+            max-h-[400px] 2xl:max-w-[1600px] 2xl:min-w-[1600px] mt-[200px]
             ">
                 <h1 className='text-mainPink font-[TT-Autonomous-Mono-Variable] font-thin max-w-fit text-[1rem]
                 text-[1.9rem] 2xl:text-[3rem]'>
@@ -35,8 +44,8 @@ const FeatureAnimation = () => {
                 <div className=' text-white text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light flex flex-wrap height-fit text-[1.7rem]
                 md:text-[1.5rem] relative
                 2xl:text-[3rem]'>
-                    <p className='text-left opacity-20'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p>
-                    <p className='text-left absolute'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p>
+                    {/* <p className='text-left opacity-20'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p> */}
+                    <p id='typingText' className='text-left absolute '></p>
                 </div>
             </div>
         </div>
