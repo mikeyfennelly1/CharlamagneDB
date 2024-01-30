c86bf94073e764e403d1b0f4888f1918de088260
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-01-30 16:11:37 +0000

    text added back in | bug where line height is decreased from animation

--- src/landing-page/2.jsx
+++ src/landing-page/2.jsx
@@ -1,36 +1,24 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from "@gsap/react";
+import BlazingFastText from './blazingFastText'
 
 gsap.registerPlugin(ScrollTrigger) 
 
 const FeatureAnimation = () => {
-
-    useGSAP(() => {
-        const outerDiv = document.getElementById('#outerDiv')
-    }, [])
-    
-    
-    // const createAnimations = () => {
-    //     // const innerDiv = document.getElementById('#innerDiv')
         useGSAP(() => {
-            gsap.to('#innerDiv', {y: 360})
     
             const tl = gsap.timeline({
                 scrollTrigger: {
                   pin: true,
                   trigger: "#outerDiv",
-                  start: "top 50%",
+                  start: "top 25%",
                   markers: true,
                   pinReparent: true,
-                //   endTrigger: "#pin-windmill-wrap",
-                  end: "bottom 50%",
+                  end: "bottom 25%",
                 },
               });
-              
-              tl.to("#innerDiv", {
-                y: -10,
-              });
+            
         })
     return (
         <>
@@ -40,7 +28,16 @@ const FeatureAnimation = () => {
             <div id='innerDiv' className="
             border-8 border-[#F00] max-h-[400px] 2xl:max-w-[1600px] 2xl:min-w-[1600px]
             ">
-                {/* <BlazingFastText></BlazingFastText> */}
+                <h1 className='text-mainPink font-[TT-Autonomous-Mono-Variable] font-thin max-w-fit text-[1rem]
+                text-[1.9rem] 2xl:text-[3rem]'>
+                    BLAZING FAST DEVELOPMENT WORKFLOW.</h1>
+                    
+                <div className=' text-white text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light flex flex-wrap height-fit text-[1.7rem]
+                md:text-[1.5rem] relative
+                2xl:text-[3rem]'>
+                    <p className='text-left opacity-20'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p>
+                    <p className='text-left absolute'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p>
+                </div>
             </div>
         </div>
 
