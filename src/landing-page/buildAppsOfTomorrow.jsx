afac9ed1e795db12af8f6a29501f0dca6ab076f7
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-02-04 16:19:19 +0000

    responsiveness issues for mobile resolved on buildAppsOfTomorrow.jsx

--- src/landing-page/buildAppsOfTomorrow.jsx
+++ src/landing-page/buildAppsOfTomorrow.jsx
@@ -25,13 +25,13 @@ const Seven = () => {
             <div id="containingBox" className="min-h-[700px] mx-w-screen mx-[20px] flex justify-center lg:min-w-[1000px] pb-[100px] font-[TT-Autonomous-Mono-Reg]">
                 <div className="min-h-[600px] border-2 w-[1100px] px-[50px] md:px-[100px] py-[100px] bg-[#0F0F0F] rounded-[30px] relative">
                     <div className="-[#F00] absolute z-10 min-h-[100%] text-white ">
-                        <h5 className="border-l-mainPink border-l-[10px] mb-[40px] max-w-[200px] py-[5px] pl-[20px]">Start here</h5>
-                        <h1 className="text-[2rem] pl-[25px] font-[TT-Travels-Next-Reg]">Build the next big thing. 
+                        <h5 className="border-l-mainPink border-l-[5px] sm:border-l-[10px] mb-[40px] max-w-[200px] py-[5px] pl-[20px]">Start here</h5>
+                        <h1 className="text-[1.5rem] sm:text-[2rem] sm:pl-[25px] font-[TT-Travels-Next-Reg] pr-[20px]">Build the next big thing. 
                         </h1>
-                        <h1 className="text-[1.5rem] pl-[25px] font-[TT-Travels-Next-Bold] text-mainPink">( Do it fast with Charlemagne )</h1>
-                        <h3 className="text-[#6b6b6b] pl-[25px] font-[TT-Autonomous-Mono-Variable] font-light text-[1.2rem] max-w-[550px]">Build the applications of tomorrow with the tools of tomorrow.</h3>
+                        <h1 className="text-[1.3rem] sm:text-[1.5rem] sm:pl-[25px] font-[TT-Travels-Next-Bold] text-mainPink pr-[20px]">( Do it fast with Charlemagne )</h1>
+                        <h3 className="text-[#6b6b6b] sm:pl-[25px] font-[TT-Autonomous-Mono-Variable] font-light text-[1rem] sm:text-[1.2rem] max-w-[550px]">Build the applications of tomorrow with the tools of tomorrow.</h3>
                         <a href={constructUrl('/pricing')}>
-                            <button className="bg-mainPink rounded-[10px] px-[40px] py-[20px] text-black ml-[25px] hover:bg-white mt-[40px]">Try for free</button>
+                            <button className="bg-mainPink rounded-[10px] px-[40px] py-[20px] text-black sm:ml-[25px] hover:bg-white mt-[20px] sm:mt-[40px]">Try for free</button>
                         </a>
                     </div>
 
