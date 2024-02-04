7372803c2eb988ca2e2a0bda29784280175d29dd
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-02-04 16:04:02 +0000

    responsiveness issues for mobile resolved on blazingFast.jsx

--- src/landing-page/blazingFast.jsx
+++ src/landing-page/blazingFast.jsx
@@ -42,20 +42,23 @@ const FeatureAnimation = () => {
               });
               
         })
+
     return (
         <>
         <div id='outerDiv' className="h-[700px]
         max-w-screen mb-[150px] flex justify-center">
         
             <div id='innerDiv' className="
-            max-h-[400px] 2xl:max-w-[1600px] 2xl:min-w-[1600px] 
+            max-h-[600px] px-[50px] sm:px-[100px] min-w-[100%] 2xl:max-w-[1600px] 2xl:min-w-[1600px] 
             ">
-                <h1 id="title" className='text-mainPink font-[TT-Autonomous-Mono-Variable] font-thin max-w-fit text-[1rem]
-                text-[1.9rem] 2xl:text-[3rem]'>
+                <h1 id="title" className='text-mainPink font-[TT-Autonomous-Mono-Variable] font-thin max-w-fit
+                text-[1.5rem]
+                md:text-[2rem]
+                lg:text-[2.4rem] 2xl:text-[3rem]'>
                     BLAZING FAST DEVELOPMENT WORKFLOW.</h1>
                     
-                <div className=' text-white text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light flex flex-wrap height-fit text-[1.7rem]
-                md:text-[1.5rem] relative
+                <div className=' text-white text-center mt-[60px] font-[TT-Autonomous-Mono-Variable] font-light flex flex-wrap height-fit
+                text-[2rem] relative
                 2xl:text-[3rem]'>
                     {/* <p className='text-left opacity-20'>Charlemagne is designed with speed in mind, in query simplicity, in an integrated AI tool for schema creation and in response times, so that you can ship a high quality app fast.</p> */}
                     <p id='typingText' className='text-left absolute'></p>
