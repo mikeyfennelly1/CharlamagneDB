ac0a90e0ac747714b50e954752a6f30e216c8597
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-03-16 14:33:40 +0000

    dead html code removed

--- src/pricing/pricingCards-Enterprise.jsx
+++ src/pricing/pricingCards-Enterprise.jsx
@@ -14,19 +14,19 @@ const CardsEnterPrise = () => {
                     <div className="grid grid-cols-2 max-w-[1000px] mx-auto mt-[100px]">
                         <div className="border-mainPink px-[50px]">
                             <ul className="">
-                                <li className="mt-[10px]">• CharlemagneDB Enterprise Server</li>
-                                <li className="mt-[10px]">• Ops Manager, the management platform for CharlemagneDB</li>
-                                <li className="mt-[10px]">• Kubernetes Operator</li>
-                                <li className="mt-[10px]">• Enterprise security features</li>
-                                <li className="mt-[10px]">• Business intelligence integration and visualization</li>
+                                <li className="mt-[10px]">CharlemagneDB Enterprise Server</li>
+                                <li className="mt-[10px]">Ops Manager, the management platform for CharlemagneDB</li>
+                                <li className="mt-[10px]">Kubernetes Operator</li>
+                                <li className="mt-[10px]">Enterprise security features</li>
+                                <li className="mt-[10px]">Business intelligence integration and visualization</li>
                             </ul>
                         </div>
                         <div className="border-mainPink px-[50px]">
                             <ul>
-                                <li className="mt-[10px]">• Commercial license</li>
-                                <li className="mt-[10px]">• Self-serve training content</li>
-                                <li className="mt-[10px]">• Rich onboarding and regular check-ins</li>
-                                <li className="mt-[10px]">• Consultative support with fast initial response goals</li>
+                                <li className="mt-[10px]">Commercial license</li>
+                                <li className="mt-[10px]">Self-serve training content</li>
+                                <li className="mt-[10px]">Rich onboarding and regular check-ins</li>
+                                <li className="mt-[10px]">Consultative support with fast initial response goals</li>
                             </ul>
                         </div>
                     </div>
@@ -42,7 +42,7 @@ const CardsEnterPrise = () => {
                                 <button className="bg-[#0F0F0F] border-[1px] py-[10px] px-[20px] rounded-[7px]">
                                     Contact Sales
                                 </button>
-                            </Link>Link
+                            </Link>
                         </div>
                     </div>
             </div>
