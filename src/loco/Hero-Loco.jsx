7fc83c727fd4f6abf2517d94101159a7622b61af
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-04-08 14:21:12 +0100

    responsiveness done for loco page laptop size

--- src/loco/Hero-Loco.jsx
+++ src/loco/Hero-Loco.jsx
@@ -9,7 +9,7 @@ const HeroLoco = () => {
             <div className="xl:grid xl:grid-cols-2 max-w-[1600px] mx-auto my-auto mt-[30vh] min-h-[1500px]">
                 <div className="pl-[100px] 2xl:pl-[50px]">
                     <h1 className="text-[2rem] 2xl:text-[2.5rem] text-white font-[TT-Autonomous-Mono-Variable] font-medium max-w-[700px]"><span className="text-mainPink font-[TT-Travels-Next-Med]">CharlemagneDB Loco.</span> <br/>The multi-cloud developer data platform.</h1>
-                    <h3 className="text-[1.2rem] 2xl:text-[1.5rem] text-white font-[TT-Autonomous-Mono-Reg] max-w-[700px] my-[20px]">An integrated suite of cloud database and data services to accelerate and simplify how you build with data.</h3>
+                    <h3 className="text-[1.2rem] 2xl:text-[1.5rem] text-white font-[TT-Autonomous-Mono-Reg] max-w-[700px] my-[20px] font-thin">An integrated suite of cloud database and data services to accelerate and simplify how you build with data.</h3>
                     <div className="pl-[20px] pt-[50px]">
                         <Link to={constructUrl("CharlamagneDB/pricing")}>
                             <button className="bg-mainPink border-2 border-mainPink font-[TT-Autonomous-Mono-Variable] text-black px-[40px] py-[15px] rounded-sm hover:bg-black hover:text-white">
